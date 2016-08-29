558a499bbd23768ca05feeb1bb68546a4629265a
troy <troy@centos.home>
2016-08-29 01:27:49 +0000

    Added IS_IP functions

--- functions.php
+++ functions.php
@@ -49,4 +49,19 @@ function db_query($query) {
 
 	return $result;
 }
+
+function is_ip($str) {
+	$ret = filter_var($str, FILTER_VALIDATE_IP);
+	return $ret;
+}
+
+function is_ipv4($str) {
+	$ret = filter_var($str, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+	return $ret;
+}
+
+function is_ipv6($str) {
+	$ret = filter_var($str, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+	return $ret;
+}
 ?>
