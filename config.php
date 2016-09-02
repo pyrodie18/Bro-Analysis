21b8f2866e636d441c2443b8b798d4dbef83867b
troy <troy@troy-newlaptop.home>
2016-09-01 22:45:46 -0400

    Broked subdomain from TLD

--- config.php
+++ config.php
@@ -55,7 +55,7 @@ define('HTTP_RESPLEN', 13);  //Responding Length
 define('HTTP_STATUSCODE', 14);  //Status Code
 
 // ** This section contains all conifguration settings for bro DNS logs ** //
-define("DNS_LOG_INSERT", "INSERT INTO bro_dns (DNS_UID, DNS_TRANSID, DNS_QUERY, DNS_CLASSNAME, DNS_TYPENAME, DNS_RESPONSECODENAME, DNS_ANSWERS) VALUES "); //Holds the overall SQL insert statement
+define("DNS_LOG_INSERT", "INSERT INTO bro_dns (DNS_UID, DNS_TRANSID, DNS_QUERY_SUBDOMAIN, DNS_QUERY_TLD, DNS_CLASSNAME, DNS_TYPENAME, DNS_RESPONSECODENAME, DNS_ANSWERS) VALUES "); //Holds the overall SQL insert statement
 define('DNS_UID', 1);  //UID
 define('DNS_TS', 0);  //Time Stamp
 define('DNS_TRANSID', 7);  //Transaction ID
