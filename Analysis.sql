8563171d3462d4a0470c75e480eb1c2daa989d4a
Troy Ward <pyrodie18@gmail.com>
2016-09-05 20:17:38 -0400

    Added SQL for SMB

--- Analysis.sql
+++ Analysis.sql
@@ -11,7 +11,7 @@ SELECT bro_http.HTTP_USERAGENT, COUNT(bro_http.HTTP_USERAGENT) as SumOfAgent, CO
 FROM bro_http
 LEFT JOIN bro_conn
 ON bro_http.HTTP_UID = bro_conn.CONN_UID
-where bro_conn.CONN_ORIGH >= INET6_ATON('10.0.0.0') AND bro_conn.CONN_ORIGH <= INET6_ATON('10.255.255.255')
+WHERE bro_conn.CONN_ORIGH >= INET6_ATON('10.0.0.0') AND bro_conn.CONN_ORIGH <= INET6_ATON('10.255.255.255')
 GROUP BY bro_http.HTTP_USERAGENT
 ORDER BY SumOfHosts DESC;
 
@@ -22,6 +22,16 @@ SELECT bro_http.HTTP_USERAGENT, COUNT(bro_http.HTTP_USERAGENT) as SumOfAgent, CO
 FROM bro_http
 LEFT JOIN bro_conn
 ON bro_http.HTTP_UID = bro_conn.CONN_UID
-where bro_conn.CONN_ORIGH >= INET6_ATON('10.0.0.0') AND bro_conn.CONN_ORIGH <= INET6_ATON('10.255.255.255')
+WHERE bro_conn.CONN_ORIGH >= INET6_ATON('10.0.0.0') AND bro_conn.CONN_ORIGH <= INET6_ATON('10.255.255.255')
 GROUP BY bro_http.HTTP_USERAGENT
 ORDER BY SumOfAgent DESC;
+
+
+/*2.  Are there any SMB connections that don't go to/from a domain controller?
+Filter BRO_CONN files with a destination port of 445 (SMB).  Display source and destination IP address and destination port number and a count of the uniq grouping*/
+SELECT INET6_NTOA(bro_conn.CONN_ORIGH) as src_ip, INET6_NTOA(bro_conn.CONN_RESPH) as dst_ip, bro_conn.CONN_RESPP, COUNT(bro_conn.CONN_RESPH) AS total_connections
+FROM bro_conn
+WHERE bro_conn.CONN_RESPP = 445
+/*Add line about exclude domain controllers*/
+GROUP BY bro_conn.CONN_ORIGH, bro_conn.CONN_RESPH
+ORDER BY total_connections DESC
